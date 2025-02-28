4c2fe1a747cdb48fb430600b0b85f6db9e5fbf96
Hyo-Kyung Lee <hyoklee@hdfgroup.org>
2025-02-28 11:49:39 -0600

    fix(vcpkg): add cte-hermes-shm port
    close #4

---
+++ ports/cte-hermes-shm/portfile.cmake
@@ -0,0 +1,33 @@
+vcpkg_from_github(
+        OUT_SOURCE_PATH SOURCE_PATH
+        REPO iowarp/cte-hermes-shm
+        REF "98c368e98d948e4f6e2fc74e48ddb7161c505eed"
+        SHA512 83b9253b2172587ac60a75f1501417d42bc0fe7045291a66aad1bb196eb7ff26d67cdd8fafce8fbeb412decd52d82a9df7fefed227a5eb8bcf212c058520bf60
+        HEAD_REF main
+)
+
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+    bench BUILD_HSHM_BENCHMARKS
+    boost BUILD_Boost_TESTS
+    cereal HERMES_ENABLE_CEREAL
+    coverage HERMES_ENABLE_COVERAGE
+    doxygen HERMES_ENABLE_DOXYGEN
+    elf HERMES_USE_ELF
+    mpi BUILD_MPI_TESTS
+    openmp BUILD_OpenMP_TESTS
+    pthread HERMES_PTHREADS_ENABLED
+    shared BUILD_SHARED_LIBS
+    test BUILD_HSHM_TESTS
+    thallium HERMES_RPC_THALLIUM
+)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+    ${FEATURE_OPTIONS}
+)
+
+vcpkg_cmake_install()
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/COPYING")
