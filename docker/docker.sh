ed8a65e3dd2efc582291f08e33c270e1ab77df55
lukemartinlogan <lukemartinlogan@gmail.com>
2025-03-24 16:31:36 -0500

    Add ext-dev

--- docker/docker.sh
+++ docker/docker.sh
@@ -4,9 +4,9 @@ docker tag iowarp-base iowarp/iowarp-base:latest
 docker build --no-cache -t iowarp-deps docker -f docker/iowarp-deps.Dockerfile
 docker tag iowarp-deps iowarp/iowarp-deps:latest
 # docker push iowarp/iowarp-deps:latest
-docker build --no-cache -t iowarp-dev docker -f docker/iowarp-dev.Dockerfile
-docker tag iowarp-dev iowarp/iowarp-dev:latest
-# docker push iowarp/iowarp-dev:latest
+docker build --no-cache -t iowarp-ext-dev docker -f docker/iowarp-ext-dev.Dockerfile
+docker tag iowarp-ext-dev iowarp/iowarp-ext-dev:latest
+# docker push iowarp/iowarp-ext-dev:latest
 docker build --no-cache -t iowarp-user docker -f docker/iowarp-user.Dockerfile
 docker tag iowarp-user iowarp/iowarp-user:latest
 # docker push iowarp/iowarp-user:latest
