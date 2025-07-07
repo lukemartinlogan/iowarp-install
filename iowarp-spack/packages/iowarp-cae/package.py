f88f81fbe4a786346528cbdd7c8338b191d9c9cb
lukemartinlogan <lukemartinlogan@gmail.com>
2025-07-07 03:26:27 -0500

    Add cae

--- iowarp-spack/packages/iowarp-cae/package.py
+++ iowarp-spack/packages/iowarp-cae/package.py
@@ -1,23 +1,27 @@
 from spack.package import *
 
 
-class IowarpCte(CMakePackage):
+class IowarpCae(CMakePackage):
     homepage = "https://grc.iit.edu/docs/hermes/main-scenario"
-    git = "https://github.com/iowarp/content-transfer-engine.git"
+    git = "https://github.com/iowarp/content-assimilation-engine.git"
 
     version(
         "main",
         branch="main",
-        git="https://github.com/iowarp/content-transfer-engine.git",
+        git="https://github.com/iowarp/content-assimilation-engine.git",
         preferred=True,
     )
     version(
         "dev", branch="dev",
-        git="https://github.com/iowarp/content-transfer-engine.git"
+        git="https://github.com/iowarp/content-assimilation-engine.git"
     )
     version("priv", branch="main",
-            git="https://github.com/iowarp/content-transfer-engine.git")
+            git="https://github.com/iowarp/content-assimilation-engine.git")
 
+    # Common variants
+    variant("posix", default=True, description="Enable POSIX adapter")
+    variant("mpiio", default=True, description="Enable MPI I/O adapter")
+    variant("stdio", default=True, description="Enable STDIO adapter")
     variant("debug", default=False, description="Build shared libraries")
     variant("vfd", default=False, description="Enable HDF5 VFD")
     variant(
@@ -26,6 +30,10 @@ class IowarpCte(CMakePackage):
         description="Do not compile the library (used for dev purposes)",
     )
     variant("depsonly", default=False, description="Only install dependencies")
+
+    # MPI variants
+    variant("mpich", default=False, description="Build with MPICH")
+    variant("openmpi", default=False, description="Build with OpenMPI")
 
     depends_on("iowarp-runtime")
     depends_on("iowarp-runtime -nocompile", when="~nocompile")
@@ -36,6 +44,7 @@ class IowarpCte(CMakePackage):
 
     depends_on('cte-hermes-shm+elf')
     depends_on('cte-hermes-shm+debug', when='+debug')
+    depends_on('cte-hermes-shm+mpiio')
     depends_on('cte-hermes-shm+vfd', when='+vfd')
     depends_on('py-ppi-jarvis-cd', type=('build'))
     depends_on('iowarp-base')
@@ -46,21 +55,30 @@ class IowarpCte(CMakePackage):
     depends_on("iowarp-runtime+cuda", when="+cuda")
     depends_on("iowarp-runtime+rocm", when="+rocm")
 
-
     def cmake_args(self):
         args = []
         if "+debug" in self.spec:
             args.append("-DCMAKE_BUILD_TYPE=Debug")
         else:
             args.append("-DCMAKE_BUILD_TYPE=Release")
+        if "+posix" in self.spec:
+            args.append("-DCAE_ENABLE_POSIX_ADAPTER=ON")
+        if "+mpiio" in self.spec:
+            args.append("-DCAE_ENABLE_MPIIO_ADAPTER=ON")
+            if "+openmpi" in self.spec:
+                args.append("-DCAE_OPENMPI=ON")
+            elif "+mpich" in self.spec:
+                args.append("-DCAE_MPICH=ON")
+        if "+stdio" in self.spec:
+            args.append("-DCAE_ENABLE_STDIO_ADAPTER=ON")
         if "+vfd" in self.spec:
-            args.append("-DHERMES_ENABLE_VFD=ON")
+            args.append("-DCAE_ENABLE_VFD=ON")
         if "+nocompile" in self.spec or "+depsonly" in self.spec:
-            args.append(self.define("HERMES_NO_COMPILE", "ON"))
+            args.append(self.define("CAE_NO_COMPILE", "ON"))
         if "+cuda" in self.spec:
-            args.append(self.define("HERMES_ENABLE_CUDA", "ON"))
+            args.append(self.define("CAE_ENABLE_CUDA", "ON"))
         if "+rocm" in self.spec:
-            args.append(self.define("HERMES_ENABLE_ROCM", "ON"))
+            args.append(self.define("CAE_ENABLE_ROCM", "ON"))
         return args
 
     def setup_run_environment(self, env):
@@ -68,5 +86,4 @@ class IowarpCte(CMakePackage):
         env.prepend_path("LD_LIBRARY_PATH", self.prefix.lib)
         env.prepend_path("PYTHONPATH", self.prefix.lib)
         env.prepend_path('CMAKE_MODULE_PATH', self.prefix.cmake)
-        env.prepend_path('CMAKE_PREFIX_PATH', self.prefix.cmake)
-        
+        env.prepend_path('CMAKE_PREFIX_PATH', self.prefix.cmake) 
--- iowarp-spack/packages/iowarp/package.py
+++ iowarp-spack/packages/iowarp/package.py
@@ -19,18 +19,12 @@ class Iowarp(Package):
     version("priv", branch="dev",
             git="https://github.com/lukemartinlogan/hermes.git")
 
-    # Common across cte-hermes-shm and hermes
+    # Common variants
     variant("posix", default=True, description="Enable POSIX adapter")
     variant("mpiio", default=True, description="Enable MPI I/O adapter")
     variant("stdio", default=True, description="Enable STDIO adapter")
     variant("debug", default=False, description="Build shared libraries")
     variant("vfd", default=False, description="Enable HDF5 VFD")
-    variant("ares", default=False, description="Enable full libfabric install")
-    variant("encrypt", default=False,
-            description="Include encryption libraries")
-    variant("compress", default=False,
-            description="Include compression libraries")
-    variant("python", default=False, description="Install python bindings")
     variant(
         "nocompile",
         default=False,
@@ -38,6 +32,10 @@ class Iowarp(Package):
     )
     variant("depsonly", default=False, description="Only install dependencies")
     variant("ppi", default=True, description="Force install ppi")
+
+    # MPI variants
+    variant("mpich", default=False, description="Build with MPICH")
+    variant("openmpi", default=False, description="Build with OpenMPI")
 
     depends_on("iowarp-cte")
     depends_on("iowarp-cte -nocompile", when="~nocompile")
@@ -47,15 +45,23 @@ class Iowarp(Package):
     depends_on("iowarp-cte@dev", when="@dev")
     
     depends_on('iowarp-cte+debug', when='+debug')
-    depends_on('iowarp-cte+ares', when='+ares')
-    depends_on('iowarp-cte+encrypt', when='+encrypt')
-    depends_on('iowarp-cte+compress', when='+compress')
-    depends_on('iowarp-cte+python', when='+python')
-
-    depends_on('iowarp-cte+posix', when='+posix')
-    depends_on('iowarp-cte+mpiio', when='+mpiio')
-    depends_on('iowarp-cte+stdio', when='+stdio')
     depends_on('iowarp-cte+vfd', when='+vfd')
+
+    # Add iowarp-cae dependencies
+    depends_on("iowarp-cae")
+    depends_on("iowarp-cae -nocompile", when="~nocompile")
+    depends_on("iowarp-cae +nocompile", when="+nocompile")
+    depends_on("iowarp-cae@main", when="@main")
+    depends_on("iowarp-cae@priv", when="@priv")
+    depends_on("iowarp-cae@dev", when="@dev")
+    
+    depends_on('iowarp-cae+debug', when='+debug')
+    depends_on('iowarp-cae+posix', when='+posix')
+    depends_on('iowarp-cae+mpiio', when='+mpiio')
+    depends_on('iowarp-cae+stdio', when='+stdio')
+    depends_on('iowarp-cae+vfd', when='+vfd')
+    depends_on('iowarp-cae+mpich', when='+mpich')
+    depends_on('iowarp-cae+openmpi', when='+openmpi')
 
     depends_on('py-ppi-jarvis-cd', when='+ppi', type=('build', 'run'))
     depends_on('py-ppi-scspkg', when='+ppi', type=('build', 'run'))
@@ -68,3 +74,5 @@ class Iowarp(Package):
     variant("rocm", default=False, description="Enable ROCm support for iowarp")
     depends_on("iowarp-cte+cuda", when="+cuda")
     depends_on("iowarp-cte+rocm", when="+rocm")
+    depends_on("iowarp-cae+cuda", when="+cuda")
+    depends_on("iowarp-cae+rocm", when="+rocm")
