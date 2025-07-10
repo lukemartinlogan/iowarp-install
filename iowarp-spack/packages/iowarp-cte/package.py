c4530a50f404a736a8b37fe5b833f69344800561
lukemartinlogan <lukemartinlogan@gmail.com>
2025-07-10 00:21:42 -0500

    Revert cte to main

--- iowarp-spack/packages/iowarp-cte/package.py
+++ iowarp-spack/packages/iowarp-cte/package.py
@@ -1,6 +1,5 @@
 from spack.package import *
 
-
 class IowarpCte(CMakePackage):
     homepage = "https://grc.iit.edu/docs/hermes/main-scenario"
     git = "https://github.com/iowarp/content-transfer-engine.git"
@@ -18,8 +17,14 @@ class IowarpCte(CMakePackage):
     version("priv", branch="main",
             git="https://github.com/iowarp/content-transfer-engine.git")
 
+    # Common across cte-hermes-shm and hermes
+    variant("posix", default=True, description="Enable POSIX adapter")
+    variant("mpiio", default=True, description="Enable MPI I/O adapter")
+    variant("stdio", default=True, description="Enable STDIO adapter")
     variant("debug", default=False, description="Build shared libraries")
+    variant("vfd", default=False, description="Enable HDF5 VFD")
     variant("ares", default=False, description="Enable full libfabric install")
+    variant("adios", default=False, description="Build Adios tests")
     variant("encrypt", default=False,
             description="Include encryption libraries")
     variant("compress", default=False,
@@ -41,7 +46,10 @@ class IowarpCte(CMakePackage):
 
     depends_on('cte-hermes-shm+elf')
     depends_on('cte-hermes-shm+debug', when='+debug')
+    depends_on('cte-hermes-shm+mpiio')
     depends_on('cte-hermes-shm+ares', when='+ares')
+    depends_on('cte-hermes-shm+vfd', when='+vfd')
+    depends_on('cte-hermes-shm+adios', when='+adios')
     depends_on('cte-hermes-shm+encrypt', when='+encrypt')
     depends_on('cte-hermes-shm+compress', when='+compress')
     depends_on('py-ppi-jarvis-cd', type=('build'))
@@ -55,13 +63,23 @@ class IowarpCte(CMakePackage):
 
 
     def cmake_args(self):
-        args = []
+        args = [self.define_from_variant("HERMES_ENABLE_PYTHON","python")]
         if "+debug" in self.spec:
             args.append("-DCMAKE_BUILD_TYPE=Debug")
         else:
             args.append("-DCMAKE_BUILD_TYPE=Release")
-        if "+ares" in self.spec:
-            args.append("-DHERMES_ENABLE_ARES=ON")
+        if "+posix" in self.spec:
+            args.append("-DHERMES_ENABLE_POSIX_ADAPTER=ON")
+        if "+mpiio" in self.spec:
+            args.append("-DHERMES_ENABLE_MPIIO_ADAPTER=ON")
+            if "openmpi" in self.spec:
+                args.append("-DHERMES_OPENMPI=ON")
+            elif "mpich" in self.spec:
+                args.append("-DHERMES_MPICH=ON")
+        if "+stdio" in self.spec:
+            args.append("-DHERMES_ENABLE_STDIO_ADAPTER=ON")
+        if "+vfd" in self.spec:
+            args.append("-DHERMES_ENABLE_VFD=ON")
         if "+compress" in self.spec:
             args.append(self.define("HERMES_ENABLE_COMPRESS", "ON"))
         if "+encrypt" in self.spec:
@@ -82,4 +100,88 @@ class IowarpCte(CMakePackage):
         env.prepend_path("PYTHONPATH", self.prefix.lib)
         env.prepend_path('CMAKE_MODULE_PATH', self.prefix.cmake)
         env.prepend_path('CMAKE_PREFIX_PATH', self.prefix.cmake)
+        
+
+
+# class IowarpCte(CMakePackage):
+#     homepage = "https://grc.iit.edu/docs/hermes/main-scenario"
+#     git = "https://github.com/iowarp/content-transfer-engine.git"
+
+#     version(
+#         "main",
+#         branch="main",
+#         git="https://github.com/iowarp/content-transfer-engine.git",
+#         preferred=True,
+#     )
+#     version(
+#         "dev", branch="dev",
+#         git="https://github.com/iowarp/content-transfer-engine.git"
+#     )
+#     version("priv", branch="main",
+#             git="https://github.com/iowarp/content-transfer-engine.git")
+
+#     variant("debug", default=False, description="Build shared libraries")
+#     variant("ares", default=False, description="Enable full libfabric install")
+#     variant("encrypt", default=False,
+#             description="Include encryption libraries")
+#     variant("compress", default=False,
+#             description="Include compression libraries")
+#     variant("python", default=False, description="Install python bindings")
+#     variant(
+#         "nocompile",
+#         default=False,
+#         description="Do not compile the library (used for dev purposes)",
+#     )
+#     variant("depsonly", default=False, description="Only install dependencies")
+
+#     depends_on("iowarp-runtime")
+#     depends_on("iowarp-runtime -nocompile", when="~nocompile")
+#     depends_on("iowarp-runtime +nocompile", when="+nocompile")
+#     depends_on("iowarp-runtime@main", when="@main")
+#     depends_on("iowarp-runtime@priv", when="@priv")
+#     depends_on("iowarp-runtime@dev", when="@dev")
+
+#     depends_on('cte-hermes-shm+elf')
+#     depends_on('cte-hermes-shm+debug', when='+debug')
+#     depends_on('cte-hermes-shm+ares', when='+ares')
+#     depends_on('cte-hermes-shm+encrypt', when='+encrypt')
+#     depends_on('cte-hermes-shm+compress', when='+compress')
+#     depends_on('py-ppi-jarvis-cd', type=('build'))
+#     depends_on('iowarp-base')
+
+#     # GPU variants
+#     variant("cuda", default=False, description="Enable CUDA support for iowarp")
+#     variant("rocm", default=False, description="Enable ROCm support for iowarp")
+#     depends_on("iowarp-runtime+cuda", when="+cuda")
+#     depends_on("iowarp-runtime+rocm", when="+rocm")
+
+
+#     def cmake_args(self):
+#         args = []
+#         if "+debug" in self.spec:
+#             args.append("-DCMAKE_BUILD_TYPE=Debug")
+#         else:
+#             args.append("-DCMAKE_BUILD_TYPE=Release")
+#         if "+ares" in self.spec:
+#             args.append("-DHERMES_ENABLE_ARES=ON")
+#         if "+compress" in self.spec:
+#             args.append(self.define("HERMES_ENABLE_COMPRESS", "ON"))
+#         if "+encrypt" in self.spec:
+#             args.append(self.define("HERMES_ENABLE_ENCRYPT", "ON"))
+#         if "+nocompile" in self.spec or "+depsonly" in self.spec:
+#             args.append(self.define("HERMES_NO_COMPILE", "ON"))
+#         if "+python" in self.spec:
+#             args.append(self.define("HERMES_ENABLE_PYTHON", "ON"))
+#         if "+cuda" in self.spec:
+#             args.append(self.define("HERMES_ENABLE_CUDA", "ON"))
+#         if "+rocm" in self.spec:
+#             args.append(self.define("HERMES_ENABLE_ROCM", "ON"))
+#         return args
+
+#     def setup_run_environment(self, env):
+#         # This is for the interceptors
+#         env.prepend_path("LD_LIBRARY_PATH", self.prefix.lib)
+#         env.prepend_path("PYTHONPATH", self.prefix.lib)
+#         env.prepend_path('CMAKE_MODULE_PATH', self.prefix.cmake)
+#         env.prepend_path('CMAKE_PREFIX_PATH', self.prefix.cmake)
         
